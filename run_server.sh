bcd306cc9e3439272e736e005a9bb06cc7861553
pld <peter@helioid.com>
2012-01-23 17:10:12 -0500

    verify

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
-cd /home/formhub/bin/formhub
+cd /home/formhub/site/formhub
 python /home/formhub/site/formhub/manage.py runserver 0.0.0.0:80
--- system_install.sh
+++ system_install.sh
@@ -1,9 +1,9 @@
 # network connection required
 ping -c 1 google.com 2>&1 | grep unknown
 if [ $? -eq 0 ]
-    then
-        echo Network connection not found. Please connect and try again
-        exit 1
+then
+    echo Network connection not found. Please connect and try again
+    exit 1
 fi
 
 # install formhub system dependencies
@@ -20,12 +20,14 @@ cd bin
 sudo chmod u+x .
 cd ~
 
-echo "/bin/bash ~/bin/bash_profile_extender.sh" >> ~/.bashrc
+# add bash profile settings
+cat ~/bin/bash_profile_extender.sh >> ~/.bashrc
 source ~/.bashrc
 
 # create a directory for formhub
 mkdir site
 cd site
+
 # clone git repo from 
 git clone https://github.com/modilabs/formhub.git -b local-node
 
@@ -33,22 +35,55 @@ git clone https://github.com/modilabs/formhub.git -b local-node
 cd formhub
 
 # install site specific requirements
-pip install -r requirements.pip
+sudo pip install -r requirements.pip
 
 # handle local_settings (somehow, if we want to...)
 # TODO
 
 # syncdb & migrate
-python manage.py syncdb
-python manage.py migrate
+python manage.py syncdb --noinput
+python manage.py migrate --noinput
 
 # sudo privileges to run server in screen
-echo 'formhub ALL=(ALL) NOPASSWD: /home/formhub/bin/run_server.sh' | sudo tee -a /etc/sudoers
+echo 'formhub ALL=(ALL) NOPASSWD: /home/formhub/bin/run_server.sh' | sudo tee -a /etc/sudoers > /dev/null
 
 # install reverse ssh cron
 echo '*/3 * * * * /bin/bash /home/formhub/bin/reverse_ssh.sh >/dev/null 2>&1' | crontab
 
 # start server on boot
+echo 'sudo -u formhub /home/formhub/bin/screen_server.sh' | sudo tee /etc/init.d/server > /dev/null
+sudo update-rc.d server defaults
 
-# start server
+# start server now
+/bin/bash screen_server.sh
+
+# start adhoc network
+/bin/bash /home/formhub/bin/adhoc.sh
+if [ $? -ne 0 ]
+then
+    # adhoc network setup failed
+    echo The server is now running at http://localhost
+    echo
+    echo Adhoc network could not be created.
+    echo Please connect to a router and use your assigned IP address
+    echo to access the server.
+else
+    # network created, add startup directive 
+    echo 'sudo -u formhub /home/formhub/bin/adhoc.sh' | sudo tee /etc/init.d/adhoc > /dev/null
+    sudo update-rc.d adhoc defaults
+    echo The server is now running at http://192.168.1.1
+    echo
+    echo Adhoc network created.
+    echo Network SSID: formhub_local
+    echo Network WEP Key: 1234567890
+fi
+
+# confirm install
+echo
+if [ -d ~/site/formhub ] && [ -f /etc/init.d/server ]
+then
+    echo Installation succeeded.
+else
+    echo Installation failed.
+fi
 
