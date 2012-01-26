12414a1cce3328c1a68c4b2d408bba6964e0f5e8
pld <peter@helioid.com>
2012-01-26 10:51:01 -0500

    also add ip like this

--- adhoc.sh
+++ adhoc.sh
@@ -7,7 +7,7 @@ sudo iwconfig eth1 mode ad-hoc
 # set channel
 sudo iwconfig eth1 channel 4
 # set ssid
-sudo iwconfig eth1 essid formhub_local
+sudo iwconfig eth1 essid formhub
 # set WEP key
 sudo iwconfig eth1 key 1234567890
 sudo iwconfig eth1 commit > /dev/null 2>&1
@@ -25,12 +25,11 @@ else
     sudo ip link set eth1 up
     # set an IP address
     sudo ip addr add 192.168.1.1/16 dev eth1
+    sudo ifconfig eth1 192.168.1.1
     # start dhcpd
     sudo /etc/init.d/dhcp3-server start
 
-    # network created, add startup directive 
-    echo 'sudo -u formhub /home/formhub/bin/adhoc.sh' | sudo tee /etc/init.d/adhoc > /dev/null
-    sudo update-rc.d adhoc defaults
+    # network created
     echo The server is now running at http://192.168.1.1
     echo
     echo Adhoc network created.
