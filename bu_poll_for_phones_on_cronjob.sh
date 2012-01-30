ab8c850739f448450ae79b91eb5f6ef17f2d7ff8
Alex Dorey <dorey415@gmail.com>
2012-01-30 18:44:22 -0500

    poll for phones

---
+++ bu_poll_for_phones_on_cronjob.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sh bu_check_mounted_drives.sh
+sleep 10
+sh bu_check_mounted_drives.sh
+sleep 10
+sh bu_check_mounted_drives.sh
+sleep 10
+sh bu_check_mounted_drives.sh
+sleep 10
+sh bu_check_mounted_drives.sh
+sleep 10
