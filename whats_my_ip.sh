63cea14ce6a163310eb457d42d19eae658de9397
Alex Dorey <dorey415@gmail.com>
2012-01-30 14:44:15 -0500

    a simple dialog box to display the IP address

---
+++ whats_my_ip.sh
@@ -0,0 +1,8 @@
+IP_ADDR=$(ifconfig | grep "192.168" | cut -c 1-34)
+if [ $? = 0 ]; then
+  IP_TEXT="Your IP Address is $IP_ADDR"
+else
+  IP_TEXT="Your IP was not found. Try running 'ifconfig'"
+fi
+
+zenity --info --text="$IP_TEXT"
