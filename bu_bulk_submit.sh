ddc2a29ab4f2769f4e36036bc2b36458bca117a9
Alex Dorey <dorey415@gmail.com>
2012-01-30 15:56:19 -0500

    bulk submit working now

--- bu_bulk_submit.sh
+++ bu_bulk_submit.sh
@@ -1,13 +1,29 @@
 echo "Submitting archive to local formhub"
-cd /home/formhub/bin/bulk_upload/
-PACKAGE_ID=`date | md5sum | cut -c 1-9`
-PACKAGE_NAME="package_$PACKAGE_ID"
-PACKAGE_ZIP="$PACKAGE_NAME.zip"
-sudo mv files $PACKAGE_NAME
-sudo zip -r $PACKAGE_ZIP $PACKAGE_NAME
+#cd /home/formhub/bin/bulk_upload/
+#PACKAGE_ID=`date | md5sum | cut -c 1-9`
+#PACKAGE_NAME="package_$PACKAGE_ID"
+#PACKAGE_ZIP="$PACKAGE_NAME.zip"
+#sudo mv files $PACKAGE_NAME
+#sudo zip -r $PACKAGE_ZIP $PACKAGE_NAME
 
-#remove this next line before deploying
-sudo mv $PACKAGE_NAME files
+FH_DESKTOP="/home/formhub/Desktop"
+ORIGIN_FILES="$FH_DESKTOP/UnloadedData"
+BACKUP_DEST="$FH_DESKTOP/PriorUploads"
+TIMESTAMP=`date +%m_%d_%H_%M_%S`
 
-curl -F "zip_submission_file=@$PACKAGE_ZIP;type=application/zip" http://localhost/formhub/bulk-submission
+PACKAGE_ZIP="$BACKUP_DEST/Submitted_$TIMESTAMP.zip"
+
+mkdir -p "$BACKUP_DEST"
+
+zip -r $PACKAGE_ZIP $ORIGIN_FILES
+
+if [ $? = 0 ]; then
+  RESULT=$(curl -F "zip_submission_file=@$PACKAGE_ZIP;type=application/zip" http://localhost/formhub/bulk-submission)
+  if [ $? = 0 ]; then
+    echo "Success! $RESULT"
+    rm -rf 
+  else
+    echo "There was a problem with your submission."
+  fi
+fi
 
