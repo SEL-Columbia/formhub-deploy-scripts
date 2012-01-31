682f6dc05f3239f6a95158747422473683413e32
pld <peter@helioid.com>
2012-01-31 10:09:54 -0500

    echos and correct site loc

--- update_from_git.sh
+++ update_from_git.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 echo Updating scripts...
 cd ~/bin
 git pull origin master
 echo Updating formhub...
-cd ~/site
+cd ~/site/formhub
 git pull origin local-node
+echo
+echo All done!
+sleep 2
