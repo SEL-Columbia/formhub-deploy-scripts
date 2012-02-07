1c89a1dd9874c761c8fc52e598fc399d1daec1d6
Alex Dorey <dorey415@gmail.com>
2012-02-07 12:10:17 -0500

    uses only the mac address of the eth1

--- bash_profile_extender.sh
+++ bash_profile_extender.sh
@@ -1,4 +1,4 @@
-export UNIQUE_ID=`ifconfig | grep HWaddr | md5sum | cut -c1-6`
+export UNIQUE_ID=`ifconfig | grep HWaddr | grep eth1 | egrep -o "..:..:..:..:..:.." | md5sum | cut -c1-6`
 SCRIPT_PATH=`readlink -f $0`
 export FORMHUB_BIN_DIR=$(dirname "$SCRIPT_PATH")
 export PATH="$PATH:$FORMHUB_BIN_DIR"
