f22e21853370a53982a363241d778feb64286d8d
Alex Dorey <dorey415@gmail.com>
2012-02-07 12:36:25 -0500

    passing port number as a parameter

--- reverse_ssh.sh
+++ reverse_ssh.sh
@@ -11,6 +11,7 @@
 #ps ax |grep "$COMMAND" |grep -v "grep" > /dev/null 2>&1 || $COMMAND > /dev/null 2>&1
 
 createTunnel() {
+    PORT_NUMBER=$1
     ssh -N -f -R linode.mvpafrica.org:2115:localhost:22 -L19922:linode.mvpafrica.org:22 formhub@linode.mvpafrica.org
     if [[ $? -eq 0 ]]; then
         echo SSH tunnel created successfully
@@ -21,7 +22,10 @@ createTunnel() {
 ## Run the 'ls' command remotely.  If it returns non-zero, then create a new connection
 /usr/bin/ssh -p 19922 formhub@localhost ls
 if [[ $? -ne 0 ]]; then
-    echo Creating new tunnel connection
-    createTunnel
+    PORT_NUMBER=$(curl --silent http://linode.mvpafrica.org:8090/get_a_port?$UNIQUE_ID)
+    if [ $PORT_NUMBER -ne -1 ]; then
+        echo Creating new tunnel connection
+        createTunnel $PORT_NUMBER
+    fi
 fi
 
